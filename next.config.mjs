249b648982d4cbb7d441a0d05d1b4a01d9ff868c
SamGreat <samantha@greatorex.org.uk>
2024-04-25 17:32:49 +0100

    Initial boilerplate commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  headers: async () => [
+    {
+      source: "/:path*",
+      headers: [
+        {
+          key: "Strict-Transport-Security",
+          value: "*",
+        },
+        {
+          key: "access-control-allow-origin",
+          value: "*",
+        },
+        {
+          key: "X-Content-Type-Options",
+          value: "*",
+        },
+        {
+          key: "Referrer-Policy",
+          value: "origin",
+        },
+        {
+          key: "Content-Security-Policy",
+          value: "frame-ancestors 'none';",
+        },
+      ],
+    },
+  ],
+};
 
 export default nextConfig;
