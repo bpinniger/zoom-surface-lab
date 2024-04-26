87ece082498be9414dd301c580feaf3f2b15a743
SamGreat <samantha@greatorex.org.uk>
2024-04-26 16:44:39 +0100

    removed class name as causes error.

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Image alt="Zoom Products" class={styles.Applogo} src="/zoom-products.png" width={600} height={300} />
+      <Image alt="Zoom Products" src="/zoom-products.png" width={600} height={300} />
       <p>Zoom Developer Ecosystem</p>
     </main>
   );
