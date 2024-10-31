f7299b377250316d676316e6f7d47fb3bb0b7298
Brendon Pinniger <90591939+bpinniger@users.noreply.github.com>
2024-11-01 07:22:24 +1000

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 "use client";
-import Image from "next/image";
+import NextImage from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Image alt="Zoom Products" src="/zoom-products.png" width={600} height={300} />
+      <NextImage alt="Zoom Products" src="/zoom-products.png" width={600} height={300} />
       <p>Zoom Developer Ecosystem</p>
     </main>
   );
